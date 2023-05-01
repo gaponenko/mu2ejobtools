43415d65e8d2a40a712282fa0fb296fafdd2aad9
Andrei Gaponenko <gandr@fnal.gov>
2023-05-01 01:11:43 -0500

    mu2ejobpars: no explicit jobtype

--- bin/mu2etools.pm
+++ bin/mu2etools.pm
@@ -16,9 +16,6 @@ use English qw( -no_match_vars ) ; # Avoids regex performance penalty
 #================================================================
 # Define strings that are used in multiple places
 
-use constant jobtype_artgen => 'artgen';
-use constant jobtype_fileinputs => 'fileinput';
-
 use constant fclkey_randomSeed => 'services.SeedService.baseSeed';
 use constant fclkey_TFileServiceFN => 'services.TFileService.fileName';
 use constant fclkey_outModFMT => 'outputs.%s.fileName';
@@ -39,8 +36,6 @@ our $VERSION = '1.00';
 
 our @EXPORT      = qw(
                       doubleQuote
-                      jobtype_artgen
-                      jobtype_fileinputs
                       fclkey_randomSeed
                       fclkey_TFileServiceFN
                       fclkey_outModFMT
